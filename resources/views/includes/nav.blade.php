08b427fcd32842eb5f9279eb60269243e54a091a
TomGieben <t.gieben@outlook.com>
2024-12-05 09:41:20 +0100

    icons to light

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -6,74 +6,74 @@
 
     <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
         <a class="dropdown-item @if (Route::currentRouteName() == 'home') active @endif" href="{{ route('home') }}">
-            <i class="fas fa-home"></i>
+            <i class="fal fa-home"></i>
             {{ __('Home') }}
         </a>
 
         <a class="dropdown-item @if (Route::has('classrooms.*')) active @endif" href="{{ route('classrooms.index') }}">
-            <i class="fas fa-chalkboard-teacher"></i>
+            <i class="fal fa-chalkboard-teacher"></i>
             {{ __('Classrooms') }}
         </a>
 
         <a class="dropdown-item @if (Route::has('courses.*')) active @endif" href="{{ route('courses.index') }}">
-            <i class="fas fa-book"></i>
+            <i class="fal fa-book"></i>
             {{ __('Courses') }}
         </a>
 
         <a class="dropdown-item @if (Route::has('criteria.*')) active @endif" href="{{ route('criteria.index') }}">
-            <i class="fas fa-list"></i>
+            <i class="fal fa-list"></i>
             {{ __('Criteria') }}
         </a>
 
         <a class="dropdown-item @if (Route::has('education-elements.*')) active @endif"
             href="{{ route('education-elements.index') }}">
-            <i class="fas fa-graduation-cap"></i>
+            <i class="fal fa-graduation-cap"></i>
             {{ __('Education Elements') }}
         </a>
 
         <a class="dropdown-item @if (Route::has('evaluation.*')) active @endif"
             href="{{ route('evaluation.index') }}">
-            <i class="fas fa-poll"></i>
+            <i class="fal fa-poll"></i>
             {{ __('Evaluation') }}
         </a>
 
         <a class="dropdown-item @if (Route::has('executions.*')) active @endif"
             href="{{ route('executions.index') }}">
-            <i class="fas fa-tasks"></i>
+            <i class="fal fa-tasks"></i>
             {{ __('Executions') }}
         </a>
 
         <a class="dropdown-item @if (Route::has('groups.*')) active @endif" href="{{ route('groups.index') }}">
-            <i class="fas fa-users"></i>
+            <i class="fal fa-users"></i>
             {{ __('Groups') }}
         </a>
 
         <a class="dropdown-item @if (Route::has('learning-objectives.*')) active @endif"
             href="{{ route('learning-objectives.index') }}">
-            <i class="fas fa-bullseye"></i>
+            <i class="fal fa-bullseye"></i>
             {{ __('Learning Objectives') }}
         </a>
 
         <a class="dropdown-item @if (Route::has('planning.*')) active @endif" href="{{ route('planning.index') }}">
-            <i class="fas fa-calendar-alt"></i>
+            <i class="fal fa-calendar-alt"></i>
             {{ __('Planning') }}
         </a>
 
         <a class="dropdown-item @if (Route::has('resources.*')) active @endif"
             href="{{ route('resources.index') }}">
-            <i class="fas fa-book-open"></i>
+            <i class="fal fa-book-open"></i>
             {{ __('Resources') }}
         </a>
 
         <a class="dropdown-item @if (Route::has('roles.*')) active @endif" href="{{ route('users.index') }}">
-            <i class="fas fa-users"></i>
+            <i class="fal fa-users"></i>
             {{ __('Users') }}
         </a>
 
         <a class="dropdown-item border-top" href="{{ route('logout') }}"
             onclick="event.preventDefault();
                          document.getElementById('logout-form').submit();">
-            <i class="fas fa-sign-out-alt"></i>
+            <i class="fal fa-sign-out-alt"></i>
             {{ __('Logout') }}
         </a>
 
