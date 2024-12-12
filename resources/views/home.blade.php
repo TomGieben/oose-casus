d5192c3c394d877671fc27b5f8f3f53682231576
TomGieben <t.gieben@outlook.com>
2024-12-12 11:12:51 +0100

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
         </div>
         <div class="col-md-4 col-12">
             <a href="{{ route('courses.index') }}" class="card mb-3 shadow-sm text-decoration-none">
-                <div class="card-body bg-primary text-white">
+                <div class="card-body bg-primary text-white rounded">
                     <i class="fas fa-book"></i>
                     {{ __('Courses') }}
                 </div>
@@ -19,7 +19,7 @@
         </div>
         <div class="col-md-4 col-12">
             <a href="{{ route('executions.index') }}" class="card mb-3 shadow-sm text-decoration-none">
-                <div class="card-body bg-success text-white">
+                <div class="card-body bg-success text-white rounded">
                     <i class="fal fa-tasks"></i>
                     {{ __('Executions') }}
                 </div>
@@ -27,7 +27,7 @@
         </div>
         <div class="col-md-4 col-12">
             <a href="{{ route('groups.index') }}" class="card mb-3 shadow-sm text-decoration-none">
-                <div class="card-body bg-info text-white">
+                <div class="card-body bg-info text-white rounded">
                     <i class="fas fa-users"></i>
                     {{ __('Groups') }}
                 </div>
@@ -35,7 +35,7 @@
         </div>
         <div class="col-md-4 col-12">
             <a href="{{ route('education-elements.index') }}" class="card mb-3 shadow-sm text-decoration-none">
-                <div class="card-body bg-warning text-white">
+                <div class="card-body bg-warning text-white rounded">
                     <i class="fal fa-graduation-cap"></i>
                     {{ __('Education Elements') }}
                 </div>
@@ -43,7 +43,7 @@
         </div>
         <div class="col-md-4 col-12">
             <a href="{{ route('criteria.index') }}" class="card mb-3 shadow-sm text-decoration-none">
-                <div class="card-body bg-danger text-white">
+                <div class="card-body bg-danger text-white rounded">
                     <i class="fal fa-list"></i>
                     {{ __('Criteria') }}
                 </div>
@@ -51,7 +51,7 @@
         </div>
         <div class="col-md-4 col-12">
             <a href="{{ route('evaluation.index') }}" class="card mb-3 shadow-sm text-decoration-none">
-                <div class="card-body bg-secondary text-white">
+                <div class="card-body bg-secondary text-white rounded">
                     <i class="fal fa-poll"></i>
                     {{ __('Evaluation') }}
                 </div>
@@ -59,7 +59,7 @@
         </div>
         <div class="col-md-4 col-12">
             <a href="{{ route('learning-objectives.index') }}" class="card mb-3 shadow-sm text-decoration-none">
-                <div class="card-body bg-dark text-white">
+                <div class="card-body bg-dark text-white rounded">
                     <i class="fal fa-bullseye"></i>
                     {{ __('Learning Objectives') }}
                 </div>
@@ -67,7 +67,7 @@
         </div>
         <div class="col-md-4 col-12">
             <a href="{{ route('plannings.index') }}" class="card mb-3 shadow-sm text-decoration-none">
-                <div class="card-body bg-light text-dark">
+                <div class="card-body bg-light text-dark rounded">
                     <i class="fal fa-calendar-alt"></i>
                     {{ __('Planning') }}
                 </div>
@@ -75,7 +75,7 @@
         </div>
         <div class="col-md-4 col-12">
             <a href="{{ route('resources.index') }}" class="card mb-3 shadow-sm text-decoration-none">
-                <div class="card-body bg-primary text-white">
+                <div class="card-body bg-primary text-white rounded">
                     <i class="fal fa-book-open"></i>
                     {{ __('Resources') }}
                 </div>
@@ -83,7 +83,7 @@
         </div>
         <div class="col-md-4 col-12">
             <a href="{{ route('users.index') }}" class="card mb-3 shadow-sm text-decoration-none">
-                <div class="card-body bg-success text-white">
+                <div class="card-body bg-success text-white rounded">
                     <i class="fal fa-users"></i>
                     {{ __('Users') }}
                 </div>
